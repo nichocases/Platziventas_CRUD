3b5fe55bb1637e93e702700fba4a45674365afbd
Castaneda, Nicholas <Nicholas.Castaneda@experian.com>
2020-02-02 17:35:37 -0500

    uso de None y for infinito

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 clients = 'pablo,ricardo,'
 
 def create_client(client_name):
@@ -24,6 +25,16 @@ def update_client(client_name,updated_client_name):
         _not_client_name()
 
 
+def search_client(client_name):
+    clients_list=clients.split(',')
+
+    for client in clients_list:
+        if client != client_name:
+            continue
+        else:
+            return True
+
+
 def delete_client(client_name):
     global clients
     if client_name in clients:
@@ -45,9 +56,20 @@ def _print_welcome():
     print('[C]reate client ')
     print('[U]pdate Client')
     print('[D]elete client')
+    print('[S]earch Client')
 
 def _get_client_name():
-    return input('What is the client name? ')
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name? ')
+        if client_name == 'exit':
+            client_name = None
+            break
+    if not client_name:
+            sys.exit()
+
+    return client_name
 
 def _not_client_name():
     print('Client is not in client list')
@@ -72,6 +94,14 @@ if __name__=='__main__':
         update_client(client_name,updated_client_name)
         list_clients()
         pass
+    elif command == 'S':
+        client_name = _get_client_name()
+        found = search_client(client_name)
+
+        if found:
+            print('The client is in the client\'s list')
+        else:
+            print('The client: {} is not in our client\'s list'.format(client_name))
     else:
         print('invalid command')
 
